03b2de071630e61fbc1d732d1894e4fd5af7da5d
MOohamedMahfouz <mouhamadmahfouz20@gmail.com>
2023-06-24 22:09:46 +0300

    first project

---
+++ Test-Laravel-Routes-main/app/Http/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $pageTitle = 'Homepage';
+        return view('welcome', compact('pageTitle'));
+    }
+}
