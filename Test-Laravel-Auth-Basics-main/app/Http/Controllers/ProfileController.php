0fb0055065352c3d505053c756a716442fd5b18a
MOohamedMahfouz <mouhamadmahfouz20@gmail.com>
2023-06-25 22:34:32 +0300

    third project

---
+++ Test-Laravel-Auth-Basics-main/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    public function show()
+    {
+        return view('auth.profile');
+    }
+
+    public function update(ProfileUpdateRequest $request)
+    {
+        // Task: fill in the code here to update name and email
+        // Also, update the password if it is set
+
+        $user = Auth::user();
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->password);
+        }
+        $user->save();
+
+        return redirect()->route('profile.show')->with('success', 'Profile updated.');
+    }
+}
