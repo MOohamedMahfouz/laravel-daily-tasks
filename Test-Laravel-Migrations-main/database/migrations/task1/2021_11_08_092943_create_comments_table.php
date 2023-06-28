a5dba387dcfb885b73e11674bcc3347a4c0905cb
MOohamedMahfouz <mouhamadmahfouz20@gmail.com>
2023-06-28 17:06:30 +0300

    fourth task

---
+++ Test-Laravel-Migrations-main/database/migrations/task1/2021_11_08_092943_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->bigInteger('comment_id')->unsigned()->nullable();
+            $table->foreign('comment_id')->references('id')->on('comments');
+            $table->string('comment_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
---
+++ Test-Laravel-Migrations-main/database/migrations/task5/2021_11_09_083121_update_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // TASK: add an if-statement in this file to NOT add column if it already exists
+        if (!Schema::hasColumn('users','name')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('name');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('surname');
+        });
+    }
+}
